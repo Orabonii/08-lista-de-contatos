e66bf9c8b068f6e83629360844e50e3f7d351555
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-05-19 18:07:25 -0300

    Add files via upload

---
+++ projeto08/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+import ListaDeContatos from './Components/Projeto08/Projeto08'; 
+import './App.css'
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <ListaDeContatos/> 
+
+    </div> 
+
+  ); 
+
+} 
+
+export default App; 
